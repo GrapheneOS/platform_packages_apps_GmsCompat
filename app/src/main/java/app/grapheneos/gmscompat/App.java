f116df7b000bdfb68fbde91362732598e0e17106
empratyush <codelab@pratyush.dev>
2022-01-30 22:00:19 +0530

    fix memory leak

--- app/src/main/java/app/grapheneos/gmscompat/App.java
+++ app/src/main/java/app/grapheneos/gmscompat/App.java
@@ -4,17 +4,19 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.lang.ref.WeakReference;
+
 public class App extends Application {
-    private static Context ctx;
+    private static WeakReference<Context> ctx;
     private static SharedPreferences preferences;
 
     public void onCreate() {
         super.onCreate();
-        ctx = getApplicationContext();
+        ctx = new WeakReference<Context>(getApplicationContext());
     }
 
     public static Context ctx() {
-        return ctx;
+        return ctx.get();
     }
 
     public static SharedPreferences preferences() {
