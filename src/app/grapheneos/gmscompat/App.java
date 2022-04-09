fa0994b71dffaea8322a8f9db5556fb4c6520648
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-04-09 16:13:51 +0300

    infrastructure for new notifications

--- src/app/grapheneos/gmscompat/App.java
+++ src/app/grapheneos/gmscompat/App.java
@@ -1,14 +1,11 @@
 package app.grapheneos.gmscompat;
 
 import android.app.Application;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.android.internal.gmscompat.GmsCompatApp;
-
-import java.util.ArrayList;
 
 public class App extends Application {
     private static Context ctx;
@@ -34,7 +31,9 @@ public class App extends Application {
                 .createDeviceProtectedStorageContext()
                 .getSharedPreferences(MAIN_PROCESS_PREFS_FILE, MODE_PRIVATE);
             Redirections.init(preferences);
-            setupNotificationChannels(ctx);
+
+            notificationManager = ctx.getSystemService(NotificationManager.class);
+            Notifications.createNotificationChannels(ctx);
         }
     }
 
@@ -53,30 +52,6 @@ public class App extends Application {
 
     public static Thread mainThread() {
         return mainThread;
-    }
-
-    public interface NotificationChannels {
-        String PERSISTENT_FG_SERVICE = "persistent_fg_service";
-        String PLAY_STORE_PENDING_USER_ACTION = "play_store_pending_user_action";
-    }
-
-    public interface NotificationIds {
-        int PERSISTENT_FG_SERVICE = 1;
-        int PLAY_STORE_PENDING_USER_ACTION = 2;
-    }
-
-    private static void setupNotificationChannels(Context ctx) {
-        ArrayList<NotificationChannel> list = new ArrayList<>();
-        String[] ids = { NotificationChannels.PERSISTENT_FG_SERVICE, NotificationChannels.PLAY_STORE_PENDING_USER_ACTION };
-        int[] titles = { R.string.persistent_fg_service_notif, R.string.play_store_pending_user_action_notif };
-
-        for (int i = 0; i < ids.length; ++i) {
-            NotificationChannel nc = new NotificationChannel(ids[i], ctx.getText(titles[i]), NotificationManager.IMPORTANCE_LOW);
-            nc.setShowBadge(false);
-            list.add(nc);
-        }
-        notificationManager = ctx.getSystemService(NotificationManager.class);
-        notificationManager.createNotificationChannels(list);
     }
 
     private static final String MAIN_PROCESS_PREFS_FILE = "prefs";
--- src/app/grapheneos/gmscompat/Const.java
+++ src/app/grapheneos/gmscompat/Const.java
@@ -1,7 +1,5 @@
 package app.grapheneos.gmscompat;
 
-import android.os.Build;
-
 public interface Const {
     boolean DEV = false;
 }
