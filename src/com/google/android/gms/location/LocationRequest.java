b52ba2390335c08fcb4da9a7faf355d13b5b15d4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-05-11 10:41:48 +0300

    bugfix: LocationRequest.durationMillis was set incorrectly in some cases
    Previous revert of new properties in GMS LocationRequest was a result of
    a misunderstanding.
    Also, remove redundant explicitFastestInterval property
    (minUpdateIntervalMillis should always be set by the client).

--- src/com/google/android/gms/location/LocationRequest.java
+++ src/com/google/android/gms/location/LocationRequest.java
@@ -17,12 +17,23 @@ public class LocationRequest extends SpReadOnly {
     @Property(1) public int priority;
     @Property(2) public long interval;
     @Property(3) public long minUpdateIntervalMillis;
-    @Property(4) public boolean explicitFastestInterval;
-    @Property(5) public long expirationTime;
+    @Property(5) public long expirationTime = Long.MAX_VALUE;
     @Property(6) public int maxUpdates;
     @Property(7) public float minUpdateDistanceMeters;
     @Property(8) public long maxUpdateDelayMillis;
     @Property(9) public boolean waitForAccurateLocation;
+    @Property(10) public long durationMillis = Long.MAX_VALUE;
+
+    /*
+    unused for now:
+
+    @Property(11) public long maxUpdateAgeMillis = -1L;
+    @Property(12) public int granularity;
+    @Property(13) public int throttleBehavior;
+    @Property(14) public String moduleId;
+    @Property(15) public boolean bypass;
+    @Property(16) public WorkSource workSource;
+     */
 
 // SafeParcel code block generated with Spoon | START
     public static final Parcelable.Creator<LocationRequest> CREATOR = new Parcelable.Creator<LocationRequest>() {
@@ -41,9 +52,6 @@ public class LocationRequest extends SpReadOnly {
                     case 3 :
                         o.minUpdateIntervalMillis = SafeParcel.readLong(ph, p);
                         continue;
-                    case 4 :
-                        o.explicitFastestInterval = SafeParcel.readBoolean(ph, p);
-                        continue;
                     case 5 :
                         o.expirationTime = SafeParcel.readLong(ph, p);
                         continue;
@@ -58,6 +66,9 @@ public class LocationRequest extends SpReadOnly {
                         continue;
                     case 9 :
                         o.waitForAccurateLocation = SafeParcel.readBoolean(ph, p);
+                        continue;
+                    case 10 :
+                        o.durationMillis = SafeParcel.readLong(ph, p);
                         continue;
                     default :
                         SafeParcel.skipProp(ph, p);
