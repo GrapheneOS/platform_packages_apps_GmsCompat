c633224f90d646d6cb9ce6f16c8152386f8b37d4
Daniel Micay <danielmicay@gmail.com>
2022-10-22 01:21:20 -0400

    config-holder: update version to 13

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         minSdk = 33
         targetSdk = 33
-        versionCode = 12
+        versionCode = 13
         versionName = versionCode.toString()
     }
 
