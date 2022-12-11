91d99b00612f6ef9f8cc144f8ebcb3830c1d5cbb
Daniel Micay <danielmicay@gmail.com>
2022-12-11 04:41:20 -0500

    config-holder: increment version to 18

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         minSdk = 33
         targetSdk = 33
-        versionCode = 17
+        versionCode = 18
         versionName = versionCode.toString()
     }
 
