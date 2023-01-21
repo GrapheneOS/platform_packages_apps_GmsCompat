80b7bcccfc09395ac463c73a6d01a66293deb194
Daniel Micay <danielmicay@gmail.com>
2023-01-21 03:39:22 -0500

    config-holder: increment version to 27

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         minSdk = 32
         targetSdk = 33
-        versionCode = 26
+        versionCode = 27
         versionName = versionCode.toString()
     }
 
