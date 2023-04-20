07d158d648bce64c64b791a5508e1b8e85547fd4
Patryk Mis <foss@patrykmis.com>
2023-04-20 06:19:43 +0200

    Update Gradle build scripts
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- config-holder/build.gradle.kts
+++ config-holder/build.gradle.kts
@@ -1,9 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0")
-    }
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
 }
