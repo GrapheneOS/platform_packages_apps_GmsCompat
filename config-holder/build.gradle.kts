ed12d2a292ed74185093db20fd3470d152d70d2e
Patryk Mis <foss@patrykmis.com>
2023-05-02 09:01:44 +0200

    Update AGP
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- config-holder/build.gradle.kts
+++ config-holder/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
 }
