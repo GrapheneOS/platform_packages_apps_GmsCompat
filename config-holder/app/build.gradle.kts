533062714dae50ccd5848845ef3efee23b7da2a0
Daniel Micay <danielmicay@gmail.com>
2023-05-24 13:02:47 -0400

    config-holder: increment version to 55

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         minSdk = 32
         targetSdk = 33
-        versionCode = 54
+        versionCode = 55
         versionName = versionCode.toString()
     }
 
