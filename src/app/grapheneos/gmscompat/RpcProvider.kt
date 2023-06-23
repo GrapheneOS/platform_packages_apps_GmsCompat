2466c9863c030f0e7ce7547b3650a589fc4b3a1a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-23 15:16:11 +0300

    rename BinderProvider to RpcProvider
    This is preparation for its use in a generic way.

--- src/app/grapheneos/gmscompat/RpcProvider.kt
+++ src/app/grapheneos/gmscompat/RpcProvider.kt
@@ -6,28 +6,31 @@ import android.os.Binder
 import android.os.Bundle
 
 import com.android.internal.gmscompat.GmsCompatApp
-import com.android.internal.gmscompat.GmsInfo
 
-class BinderProvider : AbsContentProvider() {
+class RpcProvider : AbsContentProvider() {
     override fun call(whichStr: String, arg: String?, extras: Bundle?): Bundle? {
-        val binder: Binder = when (Integer.parseInt(whichStr)) {
-            GmsCompatApp.BINDER_IGms2Gca -> {
+        val ctx = requireContext()
+        return when (Integer.parseInt(whichStr)) {
+            GmsCompatApp.RPC_GET_BINDER_IGms2Gca -> {
                 // WRITE_GSERVICES is a signature-protected permission held by GSF, GMS Core and Play Store
-                if (context.checkCallingPermission("com.google.android.providers.gsf.permission.WRITE_GSERVICES") != PackageManager.PERMISSION_GRANTED) {
-                    val pkgName = context.packageManager.getPackagesForUid(Binder.getCallingUid())!![0]
-                    if (!GmsCompat.isGmsApp(pkgName, context.userId)) {
+                if (ctx.checkCallingPermission("com.google.android.providers.gsf.permission.WRITE_GSERVICES") != PackageManager.PERMISSION_GRANTED) {
+                    val pkgName = ctx.packageManager.getPackagesForUid(Binder.getCallingUid())!![0]
+                    if (!GmsCompat.isGmsApp(pkgName, ctx.userId)) {
                         throw SecurityException()
                     }
                 }
 
-                BinderGms2Gca
+                wrapBinder(BinderGms2Gca)
             }
-            GmsCompatApp.BINDER_IClientOfGmsCore2Gca -> {
+            GmsCompatApp.RPC_GET_BINDER_IClientOfGmsCore2Gca -> {
                 // any client of GMS Core is allowed to access this binder
-                BinderClientOfGmsCore2Gca
+                wrapBinder(BinderClientOfGmsCore2Gca)
             }
             else -> throw IllegalArgumentException(whichStr)
         }
+    }
+
+    private fun wrapBinder(binder: Binder): Bundle {
         val bundle = Bundle(1)
         bundle.putBinder(GmsCompatApp.KEY_BINDER, binder)
         return bundle
