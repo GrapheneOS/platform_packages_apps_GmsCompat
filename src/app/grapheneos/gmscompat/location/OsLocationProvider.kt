bfc8e463e34f26674465cd5b1aa5050fdd7c4ca2
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-26 12:10:30 +0300

    location service: remove support for location criteria API
    It's deprecated since Android 14, with the following explanation:
    
    > Criteria based APIs are deprecated because they cannot fully capture the complexity of location
    > providers, and encourage bad development practices. Prefer to explicit select the location
    > provider of interest (LocationManager#FUSED_PROVIDER, etc), rather than relying on Criteria.

--- src/app/grapheneos/gmscompat/location/OsLocationProvider.kt
+++ src/app/grapheneos/gmscompat/location/OsLocationProvider.kt
@@ -1,6 +1,5 @@
 package app.grapheneos.gmscompat.location
 
-import android.location.Criteria
 import android.location.Location
 import android.location.LocationManager
 import android.location.provider.ProviderProperties
@@ -25,56 +24,11 @@ class OsLocationProvider(val name: String, val properties: ProviderProperties?, 
             } else {
                 LocationManager.GPS_PROVIDER
             }
-            // getBestProvider() returns FUSED_PROVIDER sometimes, which doesn't do anything useful when
-            // there's no NETWORK_PROVIDER, but exposes more potential framework bugs
-            /*
-            val criteria = Criteria()
-            when (priority) {
-                LocationRequest.PRIORITY_HIGH_ACCURACY ->
-                    criteria.accuracy = Criteria.ACCURACY_FINE
-                LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY ->
-                    criteria.accuracy = Criteria.ACCURACY_FINE
-                LocationRequest.PRIORITY_LOW_POWER ->
-                    criteria.powerRequirement = Criteria.POWER_LOW
-                else ->
-                    throw IllegalArgumentException(priority.toString())
-            }
-            return locationManager.getBestProvider(criteria, true)!!
-             */
-
             return get(client, name, granularity)
         }
 
         fun get(client: Client, granularity: Int): OsLocationProvider {
-            // see the comment above
-            /*
-            val criteria = Criteria()
-            criteria.accuracy = when (client.permission) {
-                Permission.COARSE -> {
-                    when (granularity) {
-                        LocationRequest.GRANULARITY_PERMISSION_LEVEL,
-                        LocationRequest.GRANULARITY_COARSE ->
-                            Criteria.ACCURACY_COARSE
-                        LocationRequest.GRANULARITY_FINE -> throw SecurityException()
-                        else -> throw IllegalArgumentException()
-                    }
-                }
-                Permission.FINE -> {
-                    when (granularity) {
-                        LocationRequest.GRANULARITY_COARSE ->
-                            Criteria.ACCURACY_COARSE
-                        LocationRequest.GRANULARITY_PERMISSION_LEVEL,
-                        LocationRequest.GRANULARITY_FINE ->
-                            Criteria.ACCURACY_FINE
-                        else -> throw IllegalArgumentException()
-                    }
-                }
-            }
-
-            val name: String = client.locationManager.getBestProvider(criteria, true)!!
-             */
             val name = LocationManager.GPS_PROVIDER
-
             return get(client, name, granularity)
         }
 
