ec8e0006d5a66a1bde485337bff21d34a423677d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-07-09 09:57:31 +0300

    notify user when "Nearby devices" perm is needed to connect to Wear OS

--- src/app/grapheneos/gmscompat/NotableInterface.kt
+++ src/app/grapheneos/gmscompat/NotableInterface.kt
@@ -9,6 +9,7 @@ import com.android.internal.gmscompat.GmsInfo
 enum class NotableInterface(val ifaceName: String) {
     ExposureNotificationService("com.google.android.gms.nearby.exposurenotification.internal.INearbyExposureNotificationService"),
     GamesService("com.google.android.gms.games.internal.IGamesService"),
+    WearableService("com.google.android.gms.wearable.internal.IWearableService"),
     ;
 
     fun onAcquiredByClient(callerPkg: String, processState: Int) {
@@ -38,6 +39,32 @@ enum class NotableInterface(val ifaceName: String) {
                         ctx.getString(R.string.missing_play_games_app, getApplicationLabel(ctx, callerPkg)),
                         "com.google.android.play.games")
             }
+            WearableService -> {
+                if (processState > ActivityManager.PROCESS_STATE_TOP) {
+                    return
+                }
+
+                // this service is acquired by many Google apps in background and in foreground,
+                // show notif only for foreground Wear OS companion apps
+                when (callerPkg) {
+                    // "Wear OS"
+                    "com.google.android.wearable.app",
+                    // "Google Pixel Watch"
+                    "com.google.android.apps.wear.companion",
+                        -> showGmsCoreMissingNearbyDevicesPermGeneric(ctx, callerPkg)
+                }
+
+            }
         }
+    }
+
+    fun showGmsCoreMissingNearbyDevicesPermGeneric(ctx: Context, callerPkg: String) {
+        Notifications.configurationRequired(
+                Notifications.CH_MISSING_PERMISSION,
+                ctx.getText(R.string.missing_permission),
+                ctx.getString(R.string.notif_GmsCore_missing_nearby_devices_perm_generic, getApplicationLabel(ctx, callerPkg)),
+                ctx.getText(R.string.open_settings),
+                appSettingsIntent(GmsInfo.PACKAGE_GMS_CORE, APP_INFO_ITEM_PERMISSIONS)
+        ).show(Notifications.ID_GMS_CORE_MISSING_NEARBY_DEVICES_PERMISSION)
     }
 }
