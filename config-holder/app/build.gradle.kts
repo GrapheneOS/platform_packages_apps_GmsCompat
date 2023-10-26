d6da339bb9683770969622eec8429dee6863edac
Daniel Micay <daniel.micay@grapheneos.org>
2023-10-26 12:48:42 -0400

    config-holder: increment version to 80

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         minSdk = 32
         targetSdk = 34
-        versionCode = 79
+        versionCode = 80
         versionName = versionCode.toString()
     }
 
