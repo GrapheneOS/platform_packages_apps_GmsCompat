6f1b1a58e45e02d8eaccf82cdcccaaf9f7d2130f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-10-23 23:35:47 +0300

    fix NoOpPrewarmService chain-crash in GoogleCamera

--- lib/src/app/grapheneos/gmscompat/lib/sysservice/client/GclActivityManager.java
+++ lib/src/app/grapheneos/gmscompat/lib/sysservice/client/GclActivityManager.java
@@ -1,8 +1,12 @@
 package app.grapheneos.gmscompat.lib.sysservice.client;
 
+import android.app.AppGlobals;
 import android.app.ContentProviderHolder;
 import android.app.IActivityManager;
 import android.app.IApplicationThread;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.ext.PackageId;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
@@ -25,4 +29,24 @@ class GclActivityManager extends IActivityManager.Stub.Proxy {
         }
         return res;
     }
+
+    /** @see android.app.ContextImpl#startServiceCommon */
+    @Override
+    public ComponentName startService(IApplicationThread caller, Intent service, String resolvedType, boolean requireForeground, String callingPackage, String callingFeatureId, int userId) throws RemoteException {
+        ComponentName res = super.startService(caller, service, resolvedType, requireForeground, callingPackage, callingFeatureId, userId);
+        if (AppGlobals.getInitialPackageId() == PackageId.G_CAMERA) {
+            if (res != null && "?".equals(res.getPackageName()) && service.getComponent() != null && service.getComponent().getClassName().endsWith(".NoOpPrewarmService")) {
+                // NoOpPrewarmService is a performance optimization. GoogleCamera starts it from
+                // background in some cases, which leads to a BackgroundServiceStartNotAllowedException
+                // chain-crash.
+                //
+                // GoogleCamera has privileged integration on stock OS, it's always allowed to
+                // start background services there.
+                Log.d(TAG, "ignoring failed start of NoOpPrewarmService in GoogleCamera");
+                return null;
+            }
+        }
+        return res;
+    }
+
 }
